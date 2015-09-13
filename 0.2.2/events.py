043f470b8856bb09ace448567d82dd97bb55820e
Peter Ericson <pdericson@gmail.com>
2015-09-13 20:37:11 +1000

    Update 0.2.2/events.py

--- 0.2.2/events.py
+++ 0.2.2/events.py
@@ -367,7 +367,7 @@ def main():
             handle_logs()
             timer = time.time()
 
-        time.sleep(0.01)
+        time.sleep(0.05)
 
         logs = list(filter(is_running_or_pending, logs))
 
